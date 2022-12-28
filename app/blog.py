18316e90f96c7c093fc9a48e8d4813893a1db099
Suzukiti-1029 <h40396suzujun@gmail.com>
2022-12-28 01:07:13 +0000

    ブログのBlueprint：Update

--- app/blog.py
+++ app/blog.py
@@ -39,3 +39,43 @@ def create():
       db.commit()
       return redirect(url_for('blog.index'))
   return render_template('blog/create.html')
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_requred
+def update(id):
+  post = get_post(id)
+
+  if request.method == 'POST':
+    title = request.form['title']
+    body = request.form['body']
+    error = None
+    
+    if not title:
+      error = 'Titleを入力してください'
+    if error is not None:
+      flash(error)
+    else:
+      db = get_db()
+      db.execute('''
+        update post set title = ?, body = ?
+        where id = ?''',
+        (title, body, id)
+      )
+      db.commit()
+      return redirect(url_for('blog.index'))
+  return render_template('blog/update.html', post=post)
+
+def get_post(id, check_author=True):
+  post = get_db().execute('''
+    select p.id, title, body, created, author_id, username
+    from post p join user u on p.author_id = u.id
+    where p.id = ?''',
+    (id,)
+  ).fetchone()
+  
+  if post is None:
+    abort(404, f"ID: {id}の投稿は存在しません")
+  if check_author and post['author_id'] != g.user['id'] :
+    abort(403)
+  
+  return post
